9547147f19a7ac1da64118eba8cfe6a42ecf379e
Corb G. Lois-Miruna <cti22a207@cst.uoradea.local>
2024-01-15 20:31:33 +0200

    Initial commit

---
+++ src/main/java/Caine.java
@@ -0,0 +1,23 @@
+package ro.uoradea;
+
+public class Caine{
+    private String nume;
+    private String rasa;
+
+
+    public Caine(String nume, String rasa) {
+        this.nume = nume;
+        this.rasa=rasa;
+    }
+
+    public void setNume(String nume) {
+
+        this.nume = nume;
+    }
+    public String getNume()
+    {return nume;}
+
+    public String getRasa() {
+        return rasa;
+    }
+}
